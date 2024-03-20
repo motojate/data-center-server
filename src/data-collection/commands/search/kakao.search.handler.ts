99536d83d9b9307f0e689d5a67ed308ba54ada5e
motojate <motojate@naver.com>
2024-03-20 23:18:39 +0900

    [FEAT] 다음 검색 추가

--- src/data-collection/commands/search/kakao.search.handler.ts
+++ src/data-collection/commands/search/kakao.search.handler.ts
@@ -30,50 +30,50 @@ export class KakaoSearchCommandHandler
 
     console.log(element);
 
-    // const dto = {
-    //   startDate: '2024-01-01',
-    //   endDate: '2024-03-20',
-    //   timeUnit: 'date',
-    //   keywordGroups: [
-    //     {
-    //       groupName: '주식',
-    //       keywords: ['비트코인'],
-    //     },
-    //   ],
-    // };
-    // try {
-    //   const response = await axios.post(
-    //     'https://openapi.naver.com/v1/datalab/search',
-    //     dto,
-    //     {
-    //       headers: {
-    //         'X-Naver-Client-Id': process.env.NAVER_CLIENT_ID,
-    //         'X-Naver-Client-Secret': process.env.NAVER_CLIENT_SECRET,
-    //       },
-    //       params: {
-    //         query: command.keyword,
-    //       },
-    //     },
-    //   );
-    //   const { data } = response;
-    //   console.log(data.results[0].data);
-    // } catch (e) {
-    //   console.log(e);
-    // }
+    const dto = {
+      startDate: '2024-01-01',
+      endDate: '2024-03-20',
+      timeUnit: 'date',
+      keywordGroups: [
+        {
+          groupName: '주식',
+          keywords: ['비트코인'],
+        },
+      ],
+    };
+    try {
+      const response = await axios.post(
+        'https://openapi.naver.com/v1/datalab/search',
+        dto,
+        {
+          headers: {
+            'X-Naver-Client-Id': process.env.NAVER_CLIENT_ID,
+            'X-Naver-Client-Secret': process.env.NAVER_CLIENT_SECRET,
+          },
+          params: {
+            query: command.keyword,
+          },
+        },
+      );
+      const { data } = response;
+      console.log(data.results[0].data);
+    } catch (e) {
+      console.log(e);
+    }
 
-    // const url = 'https://dapi.kakao.com/v2/search/blog';
-    // const response = await axios.get(url, {
-    //   headers: {
-    //     'Content-Type': 'application/json',
-    //     Authorization: `KakaoAK ${process.env.DAUM_REST_KEY}`,
-    //   },
-    //   params: {
-    //     query: command.keyword,
-    //   },
-    // });
-    // response.data.documents.forEach((v) => {
-    //   console.log(removeHtmlTags(v.contents));
-    // });
-    // console.log(response.data);
+    const url = 'https://dapi.kakao.com/v2/search/blog';
+    const response = await axios.get(url, {
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: `KakaoAK ${process.env.DAUM_REST_KEY}`,
+      },
+      params: {
+        query: command.keyword,
+      },
+    });
+    response.data.documents.forEach((v) => {
+      console.log(removeHtmlTags(v.contents));
+    });
+    console.log(response.data);
   }
 }
