513177ee81fdc146a8ee5ad5a506d78a84fbab52
motojate <motojate@naver.com>
2024-03-25 23:29:49 +0900

    [FEAT] 크롤링 데이터 구현 - 하드코딩으로 구현

--- src/data-collection/commands/crawl/naver-blog.crawl.handle.ts
+++ src/data-collection/commands/crawl/naver-blog.crawl.handle.ts
@@ -6,18 +6,21 @@ import puppeteer from 'puppeteer';
 export class NaverBlogCrawlCommandHandler
   implements ICommandHandler<NaverBlogCrawlCommand>
 {
-  async execute(command: NaverBlogCrawlCommand): Promise<any> {
+  private async getDateByCrawling(keyword: string): Promise<string> {
     const browser = await puppeteer.launch({ headless: false });
     const page = await browser.newPage();
     await page.setViewport({ width: 1080, height: 1024 });
     await page.goto(
-      `https://section.blog.naver.com/Search/Post.naver?pageNo=1&rangeType=ALL&orderBy=sim&keyword=${command.keyword}`,
+      `https://section.blog.naver.com/Search/Post.naver?pageNo=1&rangeType=ALL&orderBy=sim&keyword=${keyword}`,
     );
     const selector =
       '#content > section > div.area_list_search > div:nth-child(1) > div > div.info_post > div.desc > a.desc_inner > strong';
     const element = await page.waitForSelector(selector);
     await element.click();
-
-    console.log(element);
+    await page.close();
+    return element.toString();
+  }
+  async execute(command: NaverBlogCrawlCommand): Promise<any> {
+    const data = await this.getDateByCrawling(command.keyword);
   }
 }
